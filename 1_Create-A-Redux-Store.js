d0cf76ef46d0756f43dd8e455fd069d66506407e
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-11-03 16:26:00 +0530

    Create 1_Create-A-Redux-Store.js

---
+++ 1_Create-A-Redux-Store.js
@@ -0,0 +1,16 @@
+/*
+
+Create A Redux Store: Using the Redux object's createStore() method. 
+
+*/
+
+
+
+const reducer = (state = 5) => {
+  return state;
+}
+
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+let store = Redux.createStore(reducer);
